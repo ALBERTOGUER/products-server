5c802c32a5d12ef74c97e328bbf74679054a12c5
Alberto Perez <alberto.perez.1509@outlook.com>
2025-01-27 17:53:13 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+var cors = require('cors')
+const app = express();
+const { resolve } = require("path");
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
+
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.get("/products", (req, res) => {
+  res.json({
+    succes: true,
+    data: [
+      {
+        id: 1,
+
+      }
+    ]
+
+  }
+  )
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+
+});
+
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
